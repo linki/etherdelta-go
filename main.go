fb66ee9732cfa959576f8d8bd195baa3e06890c7
Martin Linkhorst <martin.linkhorst+github.com@posteo.de>
2017-11-28 20:45:48 +0100

    feat: use token registry to find available tokens

--- main.go
+++ main.go
@@ -19,28 +19,29 @@ import (
 )
 
 const (
-	version                = "v0.2.0"
-	defaultEndpoint        = "https://mainnet.infura.io"
-	defaultContractAddress = "0x8d12A197cB00D4747a1fe03395095ce2A5CC6819"
-	defaultGasPrice        = "1000000000"
-	defaultGasLimit        = "100000"
+	version              = "v0.3.0"
+	defaultEndpoint      = "https://mainnet.infura.io"
+	defaultEtherDelta    = "0x8d12A197cB00D4747a1fe03395095ce2A5CC6819"
+	defaultTokenRegistry = "0x926a74c5C36adf004C87399e65f75628b0f98D2C"
+	defaultGasPrice      = "1000000000"
+	defaultGasLimit      = "100000"
 )
 
 var (
-	endpoint        string
-	contractAddress string
-	tokenAddress    string
-	keyStorePath    string
-	passphrase      string
-	skipWithdraw    bool
-	gasPrice        int64
-	gasLimit        int64
+	endpoint             string
+	etherDeltaAddress    string
+	tokenRegistryAddress string
+	keyStorePath         string
+	passphrase           string
+	skipWithdraw         bool
+	gasPrice             int64
+	gasLimit             int64
 )
 
 func init() {
 	kingpin.Flag("endpoint", "Ethereum RPC endpoint (optional, default: https://mainnet.infura.io)").Default(defaultEndpoint).StringVar(&endpoint)
-	kingpin.Flag("contract", "EtherDelta contract address (optional, default: 0x8d12A197cB00D4747a1fe03395095ce2A5CC6819)").Default(defaultContractAddress).StringVar(&contractAddress)
-	kingpin.Flag("token", "The token's contract address (required)").Required().StringVar(&tokenAddress)
+	kingpin.Flag("etherdelta", "EtherDelta contract address (optional, default: 0x8d12A197cB00D4747a1fe03395095ce2A5CC6819)").Default(defaultEtherDelta).StringVar(&etherDeltaAddress)
+	kingpin.Flag("token-registry", "TokenRegistry contract address (optional, default: 0x926a74c5C36adf004C87399e65f75628b0f98D2C)").Default(defaultTokenRegistry).StringVar(&tokenRegistryAddress)
 	kingpin.Flag("keystore-file", "The owner's Keystore file location (required)").Required().ExistingFileVar(&keyStorePath)
 	kingpin.Flag("passphrase", "The Keystore file's passphrase (required)").Required().StringVar(&passphrase)
 	kingpin.Flag("skip-withdraw", "Don't withdraw remaining tokens (optional, default: false)").BoolVar(&skipWithdraw)
@@ -60,10 +61,23 @@ func main() {
 	}
 	conn := ethclient.NewClient(c)
 
-	// Create an instance of the EtherDelta contract at the specific address.
-	etherdelta, err := contract.NewEtherDelta(common.HexToAddress(contractAddress), conn)
+	// Create an instance of the TokenRegistry contract at the specific address.
+	tokenRegistry, err := contract.NewTokenRegistry(common.HexToAddress(tokenRegistryAddress), conn)
 	if err != nil {
-		log.Fatalf("Failed to instantiate EtherDelta contract instance: %v", err)
+		log.Fatalf("Failed to create TokenRegistry contract instance: %v", err)
+	}
+
+	// Fetch the list of registered tokens from the token registry.
+	tokenAddresses, err := tokenRegistry.GetTokenAddresses(nil)
+	if err != nil {
+		log.Fatalf("Failed to fetch registered tokens: %v", err)
+	}
+	fmt.Println("Found tokens:", len(tokenAddresses))
+
+	// Create an instance of the EtherDelta contract at the specific address.
+	etherDelta, err := contract.NewEtherDelta(common.HexToAddress(etherDeltaAddress), conn)
+	if err != nil {
+		log.Fatalf("Failed to create EtherDelta contract instance: %v", err)
 	}
 
 	// Open the provided Keystore file.
@@ -85,39 +99,44 @@ func main() {
 		log.Fatalf("Failed to decrypt Keystore file: %v", err)
 	}
 
-	// Get the owner's as well as the token's addresses.
-	owner := ownerKey.Address
-	token := common.HexToAddress(tokenAddress)
+	for _, token := range tokenAddresses {
+		// Get the token's symbol for display later.
+		_, _, symbol, _, _, _, err := tokenRegistry.GetTokenMetaData(nil, token)
+		if err != nil {
+			log.Fatalf("Failed to fetch token metadata: %v", err)
+		}
 
-	// Retrieve the owner's deposited balance of the token.
-	balance, err := etherdelta.BalanceOf(nil, token, owner)
-	if err != nil {
-		log.Fatalf("Failed to retrieve balance: %v", err)
+		// Retrieve the owner's deposited balance of the token.
+		balance, err := etherDelta.BalanceOf(nil, token, ownerKey.Address)
+		if err != nil {
+			log.Fatalf("Failed to retrieve balance: %v", err)
+		}
+		fmt.Printf("Deposited tokens of %s: %s\n", symbol, balance)
+
+		// If withdrawal is disabled or there's nothing to withdraw, continue to the
+		// next token.
+		if skipWithdraw || balance.Cmp(common.Big0) == 0 {
+			continue
+		}
+
+		// Otherwise withdraw the deposited tokens.
+		fmt.Println("Withdrawing tokens:", balance)
+
+		// The transaction needs additional metadata, e.g. the private key for
+		// authorization as well as gas price and limit.
+		opts := bind.NewKeyedTransactor(ownerKey.PrivateKey)
+		opts.GasLimit = big.NewInt(gasLimit)
+		opts.GasPrice = big.NewInt(gasPrice)
+
+		// Execute the WithdrawToken function of the EtherDelta contract given the
+		// required arguments: the token address and amount. Also provide the metadata
+		// created above in order to successfully create and sign the transaction.
+		tx, err := etherDelta.WithdrawToken(opts, token, balance)
+		if err != nil {
+			log.Fatalf("Failed to withdraw tokens: %v", err)
+		}
+
+		// Print the hash of the submitted transaction for tracking.
+		fmt.Println("Transaction hash:", tx.Hash().String())
 	}
-	fmt.Println("Deposited tokens:", balance)
-
-	// If withdrawal is disabled or there's nothing to withdraw, end the program.
-	if skipWithdraw || balance.Cmp(common.Big0) == 0 {
-		os.Exit(0)
-	}
-
-	// Otherwise withdraw the deposited tokens.
-	fmt.Println("Withdrawing tokens:", balance)
-
-	// The transaction needs additional metadata, e.g. the private key for
-	// authorization as well as gas price and limit.
-	opts := bind.NewKeyedTransactor(ownerKey.PrivateKey)
-	opts.GasLimit = big.NewInt(gasLimit)
-	opts.GasPrice = big.NewInt(gasPrice)
-
-	// Execute the WithdrawToken function of the EtherDelta contract given the
-	// required arguments: the token address and amount. Also provide the metadata
-	// created above in order to successfully create and sign the transaction.
-	tx, err := etherdelta.WithdrawToken(opts, token, balance)
-	if err != nil {
-		log.Fatalf("Failed to withdraw tokens: %v", err)
-	}
-
-	// Print the hash of the submitted transaction for tracking.
-	fmt.Println("Transaction hash:", tx.Hash().String())
 }
